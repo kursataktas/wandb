3faadd31bbf075ed4359f6ebc4bc49a988efb87e
tonyyli-wandb <tony.li@wandb.com>
2024-09-16 12:02:24 -0700

    chore(sdk): more hacking, wip

--- wandb/__init__.py
+++ wandb/__init__.py
@@ -8,6 +8,7 @@ For scripts and interactive notebooks, see https://github.com/wandb/examples.
 
 For reference documentation, see https://docs.wandb.com/ref/python.
 """
+
 __version__ = "0.18.1.dev1"
 
 from typing import Optional
@@ -120,26 +121,33 @@ log = _preinit.PreInitCallable("wandb.log", wandb_sdk.wandb_run.Run.log)  # type
 save = _preinit.PreInitCallable("wandb.save", wandb_sdk.wandb_run.Run.save)  # type: ignore
 restore = wandb_sdk.wandb_run.restore
 use_artifact = _preinit.PreInitCallable(
-    "wandb.use_artifact", wandb_sdk.wandb_run.Run.use_artifact  # type: ignore
+    "wandb.use_artifact",
+    wandb_sdk.wandb_run.Run.use_artifact,  # type: ignore
 )
 log_artifact = _preinit.PreInitCallable(
-    "wandb.log_artifact", wandb_sdk.wandb_run.Run.log_artifact  # type: ignore
+    "wandb.log_artifact",
+    wandb_sdk.wandb_run.Run.log_artifact,  # type: ignore
 )
 log_model = _preinit.PreInitCallable(
-    "wandb.log_model", wandb_sdk.wandb_run.Run.log_model  # type: ignore
+    "wandb.log_model",
+    wandb_sdk.wandb_run.Run.log_model,  # type: ignore
 )
 use_model = _preinit.PreInitCallable(
-    "wandb.use_model", wandb_sdk.wandb_run.Run.use_model  # type: ignore
+    "wandb.use_model",
+    wandb_sdk.wandb_run.Run.use_model,  # type: ignore
 )
 link_model = _preinit.PreInitCallable(
-    "wandb.link_model", wandb_sdk.wandb_run.Run.link_model  # type: ignore
+    "wandb.link_model",
+    wandb_sdk.wandb_run.Run.link_model,  # type: ignore
 )
 define_metric = _preinit.PreInitCallable(
-    "wandb.define_metric", wandb_sdk.wandb_run.Run.define_metric  # type: ignore
+    "wandb.define_metric",
+    wandb_sdk.wandb_run.Run.define_metric,  # type: ignore
 )
 
 mark_preempting = _preinit.PreInitCallable(
-    "wandb.mark_preempting", wandb_sdk.wandb_run.Run.mark_preempting  # type: ignore
+    "wandb.mark_preempting",
+    wandb_sdk.wandb_run.Run.mark_preempting,  # type: ignore
 )
 
 plot_table = _preinit.PreInitCallable(
@@ -209,6 +217,11 @@ if "dev" in __version__:
 _sentry = _Sentry()
 _sentry.setup()
 
+try:
+    from wandb.sdk import automations
+except ImportError:
+    pass
+
 
 __all__ = (
     "__version__",
@@ -242,4 +255,5 @@ __all__ = (
     "use_model",
     "link_model",
     "define_metric",
+    "automations",
 )
